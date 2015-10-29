b6972bd066aff0a57b3c183d4a6f0149b4878baf
Dave Keen <dev@ruffness.com>
2015-10-29 10:13:26 +0100

    Added a bit of error checking

--- src/ios/IAMerger.m
+++ src/ios/IAMerger.m
@@ -7,31 +7,38 @@
 - (void)merge:(CDVInvokedUrlCommand*)command
 {
     NSString* callbackId = [command callbackId];
-
+    
     NSString* imagePath = [[command arguments] objectAtIndex:0];
     NSString* audioPath = [[command arguments] objectAtIndex:1];
-
+    
     // Load the image
-    NSData* data = [NSData dataWithContentsOfFile:imagePath];
-    UIImage *uiImage = [UIImage imageWithData:data];
-
+    NSError* error;
+    NSData* data = [NSData dataWithContentsOfFile:imagePath options: 0 error: &error];
+    if (data == nil) {
+        NSLog(@"Failed to read file, error %@", error);
+        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error description]] callbackId:callbackId];
+        return;
+    }
+    
+    UIImage* uiImage = [UIImage imageWithData:data];
+    
     // Get a file to write to in the documents
     NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];   // use file system temporary directory
     NSFileManager* fileMgr = [[NSFileManager alloc] init];
     NSString* outputMoviePath;
     int i = 1;
     do { outputMoviePath = [NSString stringWithFormat:@"%@/reflection_%03d.mp4", docsPath, i++]; } while ([fileMgr fileExistsAtPath:outputMoviePath]);
-
+    
     [self.commandDelegate runInBackground:^{
         // Create the movie using Erica's hacked utility
         MovieMaker *movieMaker = [MovieMaker createMovieAtPath:outputMoviePath audioPath:audioPath frameSize:[uiImage size] fps:1];
         [movieMaker addImageToMovie:uiImage];
         [movieMaker finalizeMovie];
-
+        
         NSDictionary* fileDict = [self getMediaDictionaryFromPath:outputMoviePath];
         NSArray* fileArray = [NSArray arrayWithObject:fileDict];
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:fileArray];
-
+        
         [self.commandDelegate sendPluginResult:result callbackId:callbackId];
     }];
 }
@@ -40,16 +47,16 @@
 {
     NSFileManager* fileMgr = [[NSFileManager alloc] init];
     NSMutableDictionary* fileDict = [NSMutableDictionary dictionaryWithCapacity:5];
-
-    CDVFile *fs = [self.commandDelegate getCommandInstance:@"File"];
-
+    
+    CDVFile* fs = [self.commandDelegate getCommandInstance:@"File"];
+    
     // Get canonical version of localPath
-    NSURL *fileURL = [NSURL URLWithString:[NSString stringWithFormat:@"file://%@", fullPath]];
+    NSURL* fileURL = [NSURL URLWithString:[NSString stringWithFormat:@"file://%@", fullPath]];
     NSURL *resolvedFileURL = [fileURL URLByResolvingSymlinksInPath];
     NSString *path = [resolvedFileURL path];
-
-    CDVFilesystemURL *url = [fs fileSystemURLforLocalPath:path];
-
+    
+    CDVFilesystemURL* url = [fs fileSystemURLforLocalPath:path];
+    
     [fileDict setObject:[fullPath lastPathComponent] forKey:@"name"];
     [fileDict setObject:fullPath forKey:@"fullPath"];
     if (url) {
@@ -68,7 +75,7 @@
     NSDate* modDate = [fileAttrs fileModificationDate];
     NSNumber* msDate = [NSNumber numberWithDouble:[modDate timeIntervalSince1970] * 1000];
     [fileDict setObject:msDate forKey:@"lastModifiedDate"];
-
+    
     return fileDict;
 }
 
